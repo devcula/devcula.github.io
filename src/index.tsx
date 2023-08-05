ba75fac26a050c0cdcbec07cccbe7c5ea97cc8d6
Abhishek <devcula@outlook.com>
2023-08-05 17:41:26 +0530

    Added terminal component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TerminalController from './views/terminal';
 
-ReactDOM.render(<div> This is a new react app</div>, document.getElementById('root'));
+// ReactDOM.render(<div> This is a new react app</div>, document.getElementById('root'));
+ReactDOM.render(<TerminalController/>, document.getElementById('root'));
